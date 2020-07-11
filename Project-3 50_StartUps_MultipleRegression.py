c327fdbbd4b2af64f33a02d2b96dd1ee75e21091
sah_manish <manish.sah@debajyotigroup.com>
2020-07-11 15:07:36 +0545

    use ColumnTransformer to implement one hot encoding

--- Project-3 50_StartUps_MultipleRegression.py
+++ Project-3 50_StartUps_MultipleRegression.py
@@ -1,6 +1,6 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 dataset = pd.read_csv('10.1 50_Startups.csv')
 
@@ -8,15 +8,29 @@ x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
 # Converting Categorical data to meaningful data:
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
 labelencoder = LabelEncoder()
 x[:, 3] = labelencoder.fit_transform(x[:, 3])
-#print(x)
+
+# print first five rows
+print('labelled data===', x[0:5])
+
+# specify the column that needs to be replaced
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(), [3])],   # The column numbers to be transformed (here is [3] but can be [0, 1, 2])
+    remainder='passthrough'                                       # Leave the rest of the columns untouched
+)
 
 # Removing categorical column and replacing it with new columns:
-oneEncode = OneHotEncoder()
-x = oneEncode.fit_transform(x).toarray()
-print(x)
+x = ct.fit_transform(x)
+
+# print first five rows
+print('transformed data===', x[0:5])
+
+
+
+
+
 
 # Removing 1st extra column: Changing n to (n-1)
 
